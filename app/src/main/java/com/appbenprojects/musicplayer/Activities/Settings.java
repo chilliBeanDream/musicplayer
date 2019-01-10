5ee75a3451bbe8a8fffd1aa3871d23143d75a0f5
chilliBeanDream <ben.swannack@gmail.com>
2019-01-10 22:16:43 +0000

    Initial commit

---
+++ app/src/main/java/com/appbenprojects/musicplayer/Activities/Settings.java
@@ -0,0 +1,15 @@
+package com.appbenprojects.musicplayer.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appbenprojects.musicplayer.R;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
