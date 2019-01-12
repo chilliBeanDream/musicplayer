b77bc6675f69bc4eb6de212ff109eae03630f1be
chilliBeanDream <ben.swannack@gmail.com>
2019-01-12 16:28:49 +0000

    Add tag viewmodel

---
+++ app/src/main/java/com/appbenprojects/musicplayer/ViewModels/TagViewModel.java
@@ -0,0 +1,30 @@
+package com.appbenprojects.musicplayer.ViewModels;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.appbenprojects.musicplayer.DataAccess.MusicPlayerRepository;
+import com.appbenprojects.musicplayer.Models.Tag;
+
+import java.util.List;
+
+public class TagViewModel extends AndroidViewModel {
+	private MusicPlayerRepository musicPlayerRepository;
+	private LiveData<List<Tag>> tagList;
+	
+	public TagViewModel(Application application) {
+		super(application);
+		musicPlayerRepository = new MusicPlayerRepository(application);
+		tagList = musicPlayerRepository.getAllTags();
+	}
+	
+	public LiveData<List<Tag>> getAllTags() {
+		return tagList;
+	}
+	
+	public void insert(Tag tag) {
+		musicPlayerRepository.insert(tag);
+	}
+}
